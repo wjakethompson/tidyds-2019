d7af68f2a54243b13e3ad285066e8432c15ab033
Jake Thompson <wjakethompson@gmail.com>
2019-05-03 14:38:13 -0500

    update setup

--- setup.R
+++ setup.R
@@ -1,1 +1,5 @@
 needed_packages <- c("tidyverse", "here", "skimr", "fivethirtyeight", "babynames", "nycflights13", "maps", "viridis", "mapproj")
+
+
+install.packages("remotes")
+github_pkgs <- c("tidyr")
