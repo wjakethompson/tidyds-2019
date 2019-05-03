ebeb1e7be5685648a328ca8ffb81acbcb0e1265f
Jake Thompson <wjakethompson@gmail.com>
2019-05-03 14:52:43 -0500

    update setup script

--- setup.R
+++ setup.R
@@ -1,5 +1,11 @@
-needed_packages <- c("tidyverse", "here", "skimr", "fivethirtyeight", "babynames", "nycflights13", "maps", "viridis", "mapproj")
+# install packages from CRAN ---------------------------------------------------
+cran_pkgs <- c("tidyverse", "here", "skimr",
+               "fivethirtyeight", "babynames", "nycflights13",
+               "maps", "viridis", "mapproj",
+               "remotes")
+install.packages(cran_pkgs, repos = "https://cran.rstudio.com/",
+                 dependencies = TRUE)
 
-
-install.packages("remotes")
-github_pkgs <- c("tidyr")
+# install packages from GitHub -------------------------------------------------
+github_pkgs <- c("tidyverse/tidyr")
+remotes::install_github(github_pkgs, dependencies = TRUE, upgrade = TRUE)
