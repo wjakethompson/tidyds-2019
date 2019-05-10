780d9ed3604c47109f53e8fb41732043da97db25
Jake Thompson <wjakethompson@gmail.com>
2019-05-10 13:23:49 -0500

    update setup to use tidydscompanion

--- setup.R
+++ setup.R
@@ -1,11 +1,9 @@
 # install packages from CRAN ---------------------------------------------------
-cran_pkgs <- c("tidyverse", "here", "skimr",
-               "fivethirtyeight", "babynames", "nycflights13",
-               "maps", "viridis", "mapproj",
-               "remotes")
+cran_pkgs <- c("tidyverse", "remotes")
 install.packages(cran_pkgs, repos = "https://cran.rstudio.com/",
                  dependencies = TRUE)
 
+
 # install packages from GitHub -------------------------------------------------
-github_pkgs <- c("tidyverse/tidyr")
+github_pkgs <- c("wjakethompson/tidydscompanion")
 remotes::install_github(github_pkgs, dependencies = TRUE, upgrade = TRUE)
